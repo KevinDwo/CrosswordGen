68646c50e14b9a7c2fdf0d8d7c0d8ea26765ab61
Kevin Dworzak <dworkevin@gmail.com>
2026-03-21 20:32:42 +0100

    Remove debug mode from app.run and clean up comments in website.py

--- src/backend/website.py
+++ src/backend/website.py
@@ -27,10 +27,6 @@ def get_entries():
 def get_layout():
     return jsonify(layout)
 
-app.run(debug=True)
-
 if __name__ == '__main__':
-    # Render weist dynamisch einen Port zu. Wenn wir lokal testen, nehmen wir 5000.
     port = int(os.environ.get("PORT", 5000))
-    # host='0.0.0.0' ist wichtig, damit der Server von außen erreichbar ist
     app.run(host='0.0.0.0', port=port)
